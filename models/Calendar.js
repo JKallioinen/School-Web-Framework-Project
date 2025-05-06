b08fe17a4199572f04758f25c258f1968584f4be
JKallioinen <jimi.kallioinen@gmail.com>
2025-05-06 16:39:13 +0300

    Calendar.js: adjusted importance colors

--- models/Calendar.js
+++ models/Calendar.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const calendarSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String},
-    start: { type: Date, required: true },   // Full datetime for calendar
-    importance: { type: String, enum: ['green', 'yellow', 'red'], default: 'green' }
+    start: { type: Date, required: true },
+    importance: { type: String, enum: ['lightgreen', 'yellow', 'lightcoral'], default: 'lightgreen' }
 });
 
 module.exports = mongoose.model('Calendar', calendarSchema);
